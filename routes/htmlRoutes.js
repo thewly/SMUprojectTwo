f54b3daea8607bc78405e93c667fd02619a427ca
J91Branson <jbcod4@gmail.com>
2019-05-16 17:39:50 -0500

    added handlebars code to display all memes in the proper area, and created the ruote to get all image in HTML Routes, just gave it names that we can use when our database is created or we can change the name iin this code to match our database later

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,22 +3,21 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    db.Memes.findAll({}).then(function(dbMemes) {
       res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
+         Meme: dbMemes
       });
     });
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
+  // app.get("/example/:id", function(req, res) {
+  //   db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  //     res.render("example", {
+  //       example: dbExample
+  //     });
+  //   });
+  // });
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
