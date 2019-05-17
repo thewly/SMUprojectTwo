5778dfc1d26339f6fe94dff64d095c61292e8282
J91Branson <jbcod4@gmail.com>
2019-05-16 19:30:58 -0500

    added the html route in our htmlroutes  , and the two get routes in our apiroutes file and the meme post function for creaating a new meme, uppdated the handlebar file to show the memes in our all memes and category memes area

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,23 +2,33 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  app.get("/api/memes", function(req, res) {
+    db.Memes.findAll({}).then(function(dbMemes) {
+      res.json(dbMemes);
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+// Get memes by category
+app.get("/api/memes/:category", function(req, res) {
+  db.Memes.findOne({
+    where: {
+      catergory: req.params.catergory
+    }
+  }).then(function(dbMemes) {
+    console.log(dbMemes);
+    res.json(dbMemes);
+  });
+});
+
+
+
+
+  // Create a new Meme
+  app.post("/api/memes", function(req, res) {
+    db.Memes.create(req.body).then(function(dbMemes) {
+      res.json(dbMemes);
     });
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+  
 };
