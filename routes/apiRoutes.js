d0336fd5eb8ee34791b90711e70e0b5320f449da
Matthew Cole <colematthewg@gmail.com>
2019-05-18 13:19:45 -0500

    for submit work in progress

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,34 +1,39 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Get all examples
-  app.get("/api/memes", function(req, res) {
-    db.Memes.findAll({}).then(function(dbMemes) {
+  app.get("/api/memes", function (req, res) {
+    db.Memes.findAll({}).then(function (dbMemes) {
       res.json(dbMemes);
     });
   });
 
-// Get memes by category
-app.get("/api/memes/categories/:catergory", function(req, res) {
-  db.Memes.findOne({
-    where: {
+  // Get memes by category
+  app.get("/api/memes/categories/:catergory", function (req, res) {
+    db.Memes.findOne({
+      where: {
         catergory: req.params.catergory
-    }
-  }).then(function(dbMemes) {
-    console.log(dbMemes);
-    res.json(dbMemes);
+      }
+    }).then(function (dbMemes) {
+      console.log(dbMemes);
+      res.json(dbMemes);
+    });
   });
-});
 
 
 
 
   // Create a new Meme
-  app.post("/api/memes", function(req, res) {
-    db.Memes.create(req.body).then(function(dbMemes) {
-      res.json(dbMemes);
+  //thanks cole
+  //all your base are belong to us
+  app.post("/api/memes", function (req, res) {
+    db.Memes.create({
+      title: req.body.FormName,
+      imageUrl: req.body.FormURL,
+      category: req.body.FormCategory,
+      description: req.body.FormDesc
+    }).then(function (results) {
+      res.json(results);
     });
   });
-
-  
 };
---
+++ schema.sql
@@ -0,0 +1,4 @@
+-- Drops the Project2DB if it exists currently --
+DROP DATABASE IF EXISTS Project2DB;
+-- Creates the "Project2DB" database --
+CREATE DATABASE Project2DB;
