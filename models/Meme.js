342689fe49f8ae5b37a9b9f09c618e515e16b12e
Sean Massingill <25423602+historybro@users.noreply.github.com>
2019-05-18 13:25:06 -0500

    commit to pull

--- models/Meme.js
+++ models/Meme.js
@@ -1,7 +1,37 @@
 module.exports = function(sequelize, DataTypes) {
   var Meme = sequelize.define("Meme", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    imageUrl: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    about: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [1]
+      }
+    },
+    importance: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    category: {
+      type: DataTypes.STRING,
+      defaultValue: "Personal",
+      allowNull: false
+    }
   });
   return Meme;
 };
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,7 @@
 var db = require("../models");
 
 module.exports = function(app) {
+  
   // Get all examples
   app.get("/api/memes", function(req, res) {
     db.Memes.findAll({}).then(function(dbMemes) {
@@ -8,27 +9,48 @@ module.exports = function(app) {
     });
   });
 
-// Get memes by category
-app.get("/api/memes/categories/:catergory", function(req, res) {
-  db.Memes.findOne({
-    where: {
-        catergory: req.params.catergory
-    }
-  }).then(function(dbMemes) {
-    console.log(dbMemes);
-    res.json(dbMemes);
-  });
-});
-
-
-
-
-  // Create a new Meme
-  app.post("/api/memes", function(req, res) {
-    db.Memes.create(req.body).then(function(dbMemes) {
+  // Get memes by category
+  app.get("/api/memes/categories/:category", function(req, res) {
+    db.Memes.findOne({
+      where: {
+        category: req.params.category
+      }
+    }).then(function(dbMemes) {
+      console.log(dbMemes);
       res.json(dbMemes);
     });
   });
 
-  
+  // Create a new Meme
+  app.post("/api/memes", function(req, res) {
+    console.log(req.body);
+    db.Memes.create(req.body).then(function(results) {
+      res.redirect("/");
+    });
+  });
+
+  // Meme deletion
+  app.delete("/api/memes/:id", function(req, res) {
+    db.Memes.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(results) {
+      var obj = {
+        meme: results
+      };
+      res.render("index", obj);
+    });
+  });
+
+  //Meme update
+  app.put("/api/memes/:id", function(req, res) {
+    db.Memes.update(req.body, {
+      where: {
+        id: req.body.id
+      }
+    }).then(function() {
+      res.redirect("/");
+    });
+  });
 };
