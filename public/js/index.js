7c89a4f3e8a0e1a0ca3e1c0bc7b1353d8a2deef5
Sean Massingill <25423602+historybro@users.noreply.github.com>
2019-05-22 14:49:51 -0500

    commit to pull

--- public/js/index.js
+++ public/js/index.js
@@ -1,33 +1,49 @@
 $(document).ready(function () {
-  var memeName = $("#FormName");
-  var memeUrl = $("#FormURL");
-  var memeCat = $("#FormCategory");
-  var memeDesc = $("#FormDesc");
-  var sub = $("#AddYourMeme");
-  $(sub).on("submit", function handleFormSubmit(event) {
+
+  $("#AddYourMeme").on("submit", function handleFormSubmit(event) {
     event.preventDefault();
+    var memeName = $("#FormName");
+    var memeUrl = $("#FormURL");
+    var memeCat = $("#FormCategory");
+    var memeDesc = $("#FormDesc");
     if (!memeUrl.val().trim() && !memeName.val().trim()) {
       return;
     }
-
     var newMeme = {
       title: memeName.val().trim(),
       imageUrl: memeUrl.val().trim(),
       category: memeCat.val().trim(),
       about: memeDesc.val().trim()
     };
-
     submitMeme(newMeme);
   });
-
-  function submitMeme(Meme) {
-    $.post("/api/memes/", Meme, function() {
-      console.log("posting?");
-    }).then(function(data) {
-      console.log("test" + data);
-      //don't need reload
-      // location.reload();
-      window.location.href = "/#AllMemes";
-    });
-  }
 });
+
+function submitMeme(Meme) {
+  $.post("/api/memes/", Meme, function () {
+    console.log("posting?");
+  }).then(function(data) {
+    console.log("test" + data);
+    //don't need reload
+    location.reload();
+    window.location.href = "/#AllMemes";
+  });
+}
+
+function deleteMeme(id) {
+  $.ajax({
+    method: "DELETE",
+    url: "/api/memes/" + id
+  }).then(function() {
+    location.reload();
+    window.location.href = "/#AllMemes";
+  });
+}
+
+function handleMemeDelete() {
+  var currentMeme = $(this)
+    .parent()
+    .parent()
+    .data("post");
+  deletePost(currentPost.id);
+}
