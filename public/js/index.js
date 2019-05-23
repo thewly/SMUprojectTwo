f4f07eeb1a0a8e72b0980048801ca9db65f99ff1
Sean Massingill <25423602+historybro@users.noreply.github.com>
2019-05-23 12:17:39 -0500

    fixed add/delete meme

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,32 @@
-$(document).ready(function () {
+$(document).ready(function() {
   $("#AddYourMeme").on("submit", handleMemeSubmit);
   $(".like").on("click", handleMemeLike);
   $(".save").on("click", handleMemeSave);
   $(".delete").on("click", handleMemeDelete);
+  $("#FormCategory").on("click", handleMemeSearch);
+  $("#search-btn").on("click", function runSearch(event) {
+    console.log("search hit");
+    event.preventDefault();
+    console.log("button-test");
+    var memeSearched = $("#meme-search")
+      .val()
+      .trim();
+    var postData = {
+      memeSearched: memeSearched
+    };
+    //Make an AJAX get request to our api
+    $.get("/api/memes/search/" + memeSearched, function() {
+      console.log("posting?");
+    }).then(function(data) {
+      console.log(JSON.stringify(data));
+      //don't need reload
+      // location.reload();
+      window.location = "/api/memes/search/" + memeSearched;
+    });
+  });
 });
+
+var container = $(".meme-container");
 
 //the functions that make everything above work live here
 
@@ -99,14 +122,78 @@ function handleMemeSave() {
   console.log("Meme Saved!");
 }
 
-$("#FormCategory").on("click", function(event){
+//TEST AREA
+
+function handleMemeSearch(event) {
   event.preventDefault();
   var whatCat = $("#categorySelect").val();
-
   $.get("/api/memes/categories/" + whatCat, function(data) {
     // document.write(data);
     console.log(data);
+    buildSearch(data);
   });
-});
+}
 
-//TEST AREA
+function buildSearch(data) {
+  container.empty();
+  for (var i = 0; i < data.length; i++) {
+    var id = data[i].id;
+    var title = data[i].title;
+    var imageUrl = data[i].imageUrl;
+    var importance = data[i].importance;
+    var about = data[i].about;
+
+    var newMeme = $("<div>");
+    newMeme.addClass("col-sm-3");
+    newMeme.append(cardDeck);
+
+    var cardDeck = $("<div>");
+    cardDeck.addClass("card-deck");
+    cardDeck.attr("data-meme", id);
+    cardDeck.append(card);
+
+    var card = $("<div>");
+    card.addClass("card");
+    card.append(image);
+    card.append(cardDiv);
+
+    var cardDiv = $("<div>");
+    cardDiv.addClass("card-body");
+    cardDiv.attr("data-likes", importance);
+    cardDiv.append(name);
+    cardDiv.append(aboutText);
+    cardDiv.append(likes);
+    cardDiv.append(deleteBtn);
+    cardDiv.append(likeBtn);
+    cardDiv.append(saveBtn);
+
+    var image = $("<img>");
+    image.addClass("card-img-top");
+    image.attr("src", imageUrl);
+
+    var name = $("<h5>");
+    name.addClass("card-title");
+    name.text(title);
+
+    var aboutText = $("<p>");
+    aboutText.addClass("card-text");
+    aboutText.text(about);
+
+    var likes = $("<p>");
+    likes.addClass("card-text");
+    likes.text(importance);
+
+    var deleteBtn = $("<button>");
+    deleteBtn.addClass("btn btn-danger delete");
+    deleteBtn.html("<i class='far fa-trash-alt'></i>");
+
+    var likeBtn = $("<button>");
+    likeBtn.addClass("btn btn-danger delete");
+    likeBtn.html("<i class='far fa-thumbs-up'></i>");
+
+    var saveBtn = $("<button>");
+    saveBtn.addClass("btn btn-danger delete");
+    saveBtn.html("<i class='far fa-save'></i>");
+    container.append(newMeme);
+  }
+}
